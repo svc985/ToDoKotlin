1a2ba58340f0fb9431dcd6f7a2be141700b0c4a5
Borislav Prikić <svc985@gmail.com>
2017-11-20 15:33:00 +0100

    Item Details screen layout created

--- app/src/main/kotlin/org/prikic/todokotlin/itemdetails/ItemDetailsActivity.kt
+++ app/src/main/kotlin/org/prikic/todokotlin/itemdetails/ItemDetailsActivity.kt
@@ -2,6 +2,7 @@ package org.prikic.todokotlin.itemdetails
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_item_details.*
 import org.prikic.todokotlin.R
 import timber.log.Timber
 
@@ -9,8 +10,12 @@ class ItemDetailsActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.item_details)
+        setContentView(R.layout.activity_item_details)
 
         Timber.d("Item details activity opened")
+
+        activity_item_details_button.setOnClickListener { _ ->
+            Timber.d("clicked save/edit button")
+        }
     }
 }
