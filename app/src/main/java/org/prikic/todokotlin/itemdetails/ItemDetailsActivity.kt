cbd3f455c7f0b0c70a65831e8e02b02c0d782bdb
Borislav Prikić <svc985@gmail.com>
2017-11-20 21:00:07 +0100

    Minor adjustments

--- app/src/main/java/org/prikic/todokotlin/itemdetails/ItemDetailsActivity.kt
+++ app/src/main/java/org/prikic/todokotlin/itemdetails/ItemDetailsActivity.kt
@@ -1,12 +1,16 @@
 package org.prikic.todokotlin.itemdetails
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import kotlinx.android.synthetic.main.activity_item_details.*
 import org.prikic.todokotlin.R
 import timber.log.Timber
 
-class ItemDetailsActivity: AppCompatActivity() {
+class ItemDetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,8 +18,28 @@ class ItemDetailsActivity: AppCompatActivity() {
 
         Timber.d("Item details activity opened")
 
-        activity_item_details_button.setOnClickListener { _ ->
-            Timber.d("clicked save/edit button")
+        activity_item_details_edit_text.setOnEditorActionListener { v, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                activity_item_details_button.performClick()
+                hideKeyboard(v)
+                true
+            } else {
+                false
+            }
         }
+
+        activity_item_details_button.setOnClickListener { _ ->
+            activity_item_details_edit_text.clearFocus()
+            submit()
+        }
+    }
+    
+    private fun hideKeyboard(v: View) {
+        val imm = v.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(v.windowToken, 0)
+    }
+
+    private fun submit() {
+        Timber.d("clicked save/edit button")
     }
 }
