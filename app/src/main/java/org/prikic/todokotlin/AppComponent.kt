6eeb10b2d994a491d2bee3af97a5fa88300cfaec
Borislav Prikić <svc985@gmail.com>
2017-11-24 15:21:15 +0100

    DI introduced for ItemDetails Screen, Arch Components library introduced

--- app/src/main/java/org/prikic/todokotlin/AppComponent.kt
+++ app/src/main/java/org/prikic/todokotlin/AppComponent.kt
@@ -1,10 +1,13 @@
 package org.prikic.todokotlin
 
 import dagger.Component
+import org.prikic.todokotlin.itemdetails.ItemDetailsViewModel
+import org.prikic.todokotlin.itemdetails.ToDoRepositoryModule
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = arrayOf(AppModule::class))
+@Component(modules = arrayOf(AppModule::class, ToDoRepositoryModule::class))
 interface AppComponent {
     fun inject(app: App)
+    fun inject(itemDetailsViewModel: ItemDetailsViewModel)
 }
--- app/src/main/java/org/prikic/todokotlin/itemdetails/ItemDetailsActivity.kt
+++ app/src/main/java/org/prikic/todokotlin/itemdetails/ItemDetailsActivity.kt
@@ -1,5 +1,6 @@
 package org.prikic.todokotlin.itemdetails
 
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -8,15 +9,20 @@ import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import kotlinx.android.synthetic.main.activity_item_details.*
 import org.prikic.todokotlin.R
+import org.prikic.todokotlin.data.model.ToDo
 import timber.log.Timber
 
 class ItemDetailsActivity : AppCompatActivity() {
+
+    private var itemDetailsVM: ItemDetailsViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_details)
 
         Timber.d("Item details activity opened")
+
+        itemDetailsVM = ViewModelProviders.of(this).get(ItemDetailsViewModel::class.java)
 
         activity_item_details_edit_text.setOnEditorActionListener { v, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
@@ -40,6 +46,30 @@ class ItemDetailsActivity : AppCompatActivity() {
     }
 
     private fun submit() {
-        Timber.d("clicked save/edit button")
+        //Timber.d("clicked save/edit button")
+
+        val isInputValid = validateInput()
+        if (!isInputValid) return
+
+        val weekDay = activity_item_details_spinner.selectedItem.toString()
+        val toDoText = activity_item_details_edit_text.text.toString()
+        val toDo = ToDo(weekDay = weekDay, toDoText = toDoText)
+        itemDetailsVM?.saveToDo(toDo)
+
     }
+
+    private fun validateInput() : Boolean {
+       if (activity_item_details_spinner.selectedItemPosition == 0) {
+           Timber.e("First item can't be selected")
+           return false
+       }
+
+        if (activity_item_details_edit_text.length() == 0) {
+            Timber.e("Edit text can't be empty")
+            return false
+        }
+        return true
+    }
+
+
 }
